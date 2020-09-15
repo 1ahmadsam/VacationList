4ba085625007f67ab3c19f35286f30ff8c1bfb45
1ahmadsam <samiahmad360@hotmail.com>
2020-09-15 07:42:59 -0400

    added, table, api, and switch for theme

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
+//API KEY
+const API_KEY = '415095d6d53abdc56ed23ecd58706b71';
 //event listeners
 window.onload = function () {
   document.querySelector('#vacation-form').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const country = document.getElementById('country').value;
+    // const country = document.getElementById('country').value;
     const city = document.getElementById('city').value;
 
-    if (country === '' || city === '') {
+    if (city === '') {
       document.getElementById('result').innerHTML = showDialog(
         'warning',
-        'enter in the information correctly'
+        'enter in a city'
       );
     } else {
       document.getElementById('result').innerHTML = showDialog(
         'success',
         'nice job'
       );
+      getWeatherData(city);
     }
-    console.log('hello:', country, city);
+    console.log('hello:', city);
+  });
+};
+
+/**
+ * change websites theme to dark
+ */
+changeWebsiteTheme = () => {
+  const checkBox = document.getElementById('check');
+  if (checkBox.checked == true) {
+    console.log('checked');
+  } else {
+    console.log('not checked');
+  }
+};
+
+/**
+ * get weather data
+ */
+getWeatherData = (city) => {
+  const URL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=7&appid=${API_KEY}`;
+  return fetch(URL).then((res) => {
+    console.log(res.json);
   });
 };
 
 /**
  *  display success or error message depending on inpout values
  */
-function showDialog(dialogType, text) {
+showDialog = (dialogType, text) => {
   let dialog;
   switch (dialogType) {
     case 'warning':
@@ -37,4 +62,4 @@ function showDialog(dialogType, text) {
   dialog += text;
   dialog += '</div>';
   return dialog;
-}
+};
