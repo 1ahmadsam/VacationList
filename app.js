31f78eab593c0f4543680742ee51ba790d8036e4
1ahmadsam <samiahmad360@hotmail.com>
2020-09-20 19:13:26 -0400

    got weather type working, colors and metric units is left

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 //API KEY
 const API_KEY = 'a4d6ef5f73e193501bb217d15ab890d7';
 let UNITS = 'metric';
-let currentUnits = UNITS === 'metric' ? 'degrees' : 'fahrenheit';
-
+let currentUnits = UNITS === 'metric' ? '°C' : '°F';
+const weatherCodes = [
+  'Clouds',
+  'Clear',
+  'Snow',
+  'Rain',
+  'Drizzle',
+  'Thunderstorm',
+];
 //chart
 
 let myChart;
+let radarChart;
 //event listeners
 window.onload = function () {
   document.querySelector('#vacation-form').addEventListener('submit', (e) => {
@@ -67,20 +75,42 @@ const dynamicColors = () => {
 };
 
 /**
+ * given list of weather types, count occurence
+ */
+const countWeatherType = (weatherTypes) => {
+  let listOfOccurences = [];
+
+  weatherCodes.forEach((weather) => {
+    let i = 0;
+    weatherTypes.forEach((type) => {
+      if (type === weather) {
+        i += 1;
+      }
+    });
+    listOfOccurences.push(i);
+  });
+  return listOfOccurences;
+};
+
+/**
  * create table with weather data
  *
  */
 createWeatherTable = (data) => {
   let weatherTableContent = '';
   weatherTableContent += `<tr>
-  <td><canvas id="${data.city.name}-chart" width="400" height="300"></canvas></td></tr>`;
+  <td><canvas id="${data.city.name}-chart" width="400" height="300"></canvas></td></tr><tr>
+  <td><canvas id="radar-chart" width="400" height="300"></canvas></td></tr>`;
   document
     .getElementById('weather-table')
     .insertAdjacentHTML('beforeend', weatherTableContent);
-  const { dates, temps, feels_temps } = transformWeatherData(data);
+  const { dates, temps, feels_temps, weather } = transformWeatherData(data);
+
+  //weather type
+  const weatherListOccurence = countWeatherType(weather);
 
   const ctx = document.getElementById(`${data.city.name}-chart`);
-  console.log(dates, temps);
+  console.log(weather);
   myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -90,14 +120,8 @@ createWeatherTable = (data) => {
           label: `${data.city.name}`,
           data: temps,
           fill: false,
-          borderColor: '#FEB100',
+          borderColor: 'rgba(255,99,132,1)',
         },
-        // {
-        //   label: 'feels like',
-        //   data: feels_temps,
-        //   fill: false,
-        //   borderColor: '#5cadfa',
-        // },
       ],
     },
     options: {
@@ -107,17 +131,52 @@ createWeatherTable = (data) => {
       },
     },
   });
+  radarChart = new Chart(document.getElementById('radar-chart'), {
+    type: 'radar',
+    data: {
+      labels: weatherCodes,
+      datasets: [
+        {
+          label: `${data.city.name}`,
+          fill: true,
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          data: weatherListOccurence,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Weather by Type',
+      },
+    },
+  });
 };
 
 const updateWeatherTable = (data) => {
-  const { dates, temps, feels_temps } = transformWeatherData(data);
+  const { dates, temps, feels_temps, weather } = transformWeatherData(data);
+
+  //weather type
+  const weatherListOccurence = countWeatherType(weather);
+  const newColor = dynamicColors();
+
   myChart.data.datasets.push({
     label: `${data.city.name}`,
     data: temps,
     fill: false,
-    borderColor: dynamicColors(),
+    borderColor: newColor,
+  });
+  radarChart.data.datasets.push({
+    label: `${data.city.name}`,
+    data: weatherListOccurence,
+    fill: true,
+
+    backgroundColor: 'rgba(255,99,132,0.2)',
+    borderColor: newColor,
   });
   myChart.update();
+  radarChart.update();
 };
 console.log('rand color', dynamicColors());
 /**
@@ -152,12 +211,14 @@ transformWeatherData = (data) => {
   let daily_temps = [];
   let feels_temps = [];
   let daily_date = [];
+  let weather_list = [];
   let x = 0;
   for (weather of weather_daily) {
     if (x % 4 === 0) {
       feels_temps.push(weather.main.feels_like);
       daily_temps.push(weather.main.temp);
       daily_date.push(weather.dt_txt);
+      weather_list.push(weather.weather[0].main);
     }
     x += 1;
   }
@@ -165,6 +226,7 @@ transformWeatherData = (data) => {
     dates: transformDates(daily_date),
     temps: daily_temps,
     feels_temps: feels_temps,
+    weather: weather_list,
   };
 };
 
